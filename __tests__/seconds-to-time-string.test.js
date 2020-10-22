b19923ce7ce76686e08b21a72f14ac4e31022649
karben86 <benjaminkarst86@gmail.com>
2020-10-22 09:33:33 +0100

    solved seconds-to-time-string problem

--- __tests__/seconds-to-time-string.test.js
+++ __tests__/seconds-to-time-string.test.js
@@ -5,30 +5,30 @@ describe('secondsToTimeString()', () => {
     expect(secondsToTimeString(1)).toBe('1 second')
   });
   it('', () => {
-    expect(secondsToTimeString(2)).toBe("2 seconds")
+    expect(secondsToTimeString(2)).toBe('2 seconds')
   });
   it('', () => {
-    expect(secondsToTimeString(60)).toBe("1 minute")
+    expect(secondsToTimeString(60)).toBe('1 minute')
   });
   it('', () => {
-    expect(secondsToTimeString(120)).toBe("2 minutes")
+    expect(secondsToTimeString(120)).toBe('2 minutes')
   });
   it('', () => {
-    expect(secondsToTimeString(121)).toBe("2 minutes and 1 second")
+    expect(secondsToTimeString(121)).toBe('2 minutes and 1 second')
   });
   it('', () => {
-    expect(secondsToTimeString(3660)).toBe("1 hour and 1 minute")
+    expect(secondsToTimeString(3660)).toBe('1 hour and 1 minute')
   });
   it('', () => {
-    expect(secondsToTimeString(3661)).toBe("1 hour, 1 minute and 1 second")
+    expect(secondsToTimeString(3661)).toBe('1 hour, 1 minute and 1 second')
   });
   it('', () => {
-    expect(secondsToTimeString(31626061)).toBe("1 year, 1 day, 1 hour, 1 minute and 1 second")
+    expect(secondsToTimeString(31626061)).toBe('1 year, 1 day, 1 hour, 1 minute and 1 second')
   });
   it('', () => {
-    expect(secondsToTimeString(31572000)).toBe("1 year and 10 hours")
+    expect(secondsToTimeString(31572000)).toBe('1 year and 10 hours')
   });
   it('', () => {
-    expect(secondsToTimeString(127702942)).toBe("4 years, 18 days, 1 hour, 2 minutes and 22 seconds")
+    expect(secondsToTimeString(127702942)).toBe('4 years, 18 days, 1 hour, 2 minutes and 22 seconds')
   });
 });
--- seconds-to-time-string.js
+++ seconds-to-time-string.js
@@ -2,20 +2,18 @@
 const secondsToTimeString = (seconds) => {
   // Setting up an object that converts seconds into the relevant time unit which can be used to look up each time building block.
   const secondsObj = { year: 31536000, day: 86400, hour: 3600, minute: 60, second: 1 };
-  let resultStr = "";
+  let resultStr = '';
   let calc = 0;
 
   // looping throug the secondsObj to look up whether each category is relevant to the current scenario
   for (var time in secondsObj) {
-    
-    // Only includes the current time unit if the amount of seconds left over is more than the time unit converted to seconds 
+    // Only includes the current time unit if the amount of seconds left over is more than the time unit converted to seconds
     if (seconds >= secondsObj[time]) {
-
       // When we include a time unit we only want to show full units i.e. 3 days or 1 month etc. Therefore any amount leftover is truncated i.e. 4.12 hours becomes 4 hours.
-      calc = Math.trunc(seconds/secondsObj[time]);
+      calc = Math.trunc(seconds / secondsObj[time]);
       // Each time unit in the end result is separated by a comma and a space i.e. 2 days, 1 hour so we concatenate this to the previous result as well as
       // the full unit of time calculated above (calc) as well as the name of that unit (time).
-      // When we have more than one full unit of the current time than we add an s to the current time i.e. 2 year becomes 2 years 
+      // When we have more than one full unit of the current time than we add an s to the current time i.e. 2 year becomes 2 years
       resultStr += `, ${calc} ${time}${'s'.repeat(!!(calc > 1))}`;
 
       // Since we have now dealt with all the full units of time that can be extracted from the current amount of seconds we want to move on to the next time unit.
@@ -30,7 +28,7 @@ const secondsToTimeString = (seconds) => {
   // I.e. instead of , 3 hours, 2 minutes we want to show 3 hours, 2 minutes which is why we need to slice off the first two characters.
   // Moreover we want to show " and " instead of the final comma and space irrespective of what the final unit of time is to make it more readable, which is why we have to
   // replace the final comma and space accordingly.
-  return resultStr.slice(2).replace(/,([^,]*)$/,' and'+'$1');
+  return resultStr.slice(2).replace(/,([^,]*)$/, ' and' + '$1');
 };
 
 module.exports = { secondsToTimeString };
