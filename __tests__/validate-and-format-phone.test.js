f2a7e12e88291fd804c57cafb492330595a95c98
karben86 <benjaminkarst86@gmail.com>
2020-10-26 08:54:16 +0000

    solved validate and format phone problem

--- __tests__/validate-and-format-phone.test.js
+++ __tests__/validate-and-format-phone.test.js
@@ -2,24 +2,24 @@ const { validateAndFormatPhone } = require('../validate-and-format-phone');
 
 describe('validateAndFormatPhone()', () => {
   it('', () => {
-    expect(validateAndFormatPhone("08559912983")).toBe("Invalid phone number")
+    expect(validateAndFormatPhone('08559912983')).toBe('Invalid phone number')
   });
   it('', () => {
-    expect(validateAndFormatPhone("Not a Number")).toBe("Invalid phone number")
+    expect(validateAndFormatPhone('Not a Number')).toBe('Invalid phone number')
   });
   it('', () => {
-    expect(validateAndFormatPhone("+2082394891246")).toBe("Invalid phone number")
+    expect(validateAndFormatPhone('+2082394891246')).toBe('Invalid phone number')
   });
   it('', () => {
-    expect(validateAndFormatPhone("01616603202")).toBe("+441616603202")
+    expect(validateAndFormatPhone('01616603202')).toBe('+441616603202')
   });
   it('', () => {
-    expect(validateAndFormatPhone("00441616603202")).toBe("+441616603202")
+    expect(validateAndFormatPhone('00441616603202')).toBe('+441616603202')
   });
   it('', () => {
-    expect(validateAndFormatPhone("07540706000")).toBe("+447540706000")
+    expect(validateAndFormatPhone('07540706000')).toBe('+447540706000')
   });
   it('', () => {
-    expect(validateAndFormatPhone("+447540706000")).toBe("+447540706000")
+    expect(validateAndFormatPhone('+447540706000')).toBe('+447540706000')
   });
 });
