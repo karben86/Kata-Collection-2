58b854e041364b24d08595332809c55730d7e4b8
karben86 <benjaminkarst86@gmail.com>
2020-10-26 09:32:26 +0000

    sovled unique and ordered problem

--- __tests__/unique-and-ordered.test.js
+++ __tests__/unique-and-ordered.test.js
@@ -2,6 +2,12 @@ const { uniqueAndOrdered } = require('../unique-and-ordered')
 
 describe('uniqueAndOrdered()', () => {
   it('', () => {
-    // first test here
+    expect(uniqueAndOrdered([1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 1, 1])).toEqual([1, 2, 3, 1]);
+  })
+  it('', () => {
+    expect(uniqueAndOrdered('AaAAABBBCCCc')).toEqual(['A', 'a', 'A', 'B', 'C', 'c']);
+  })
+  it('', () => {
+    expect(uniqueAndOrdered('nnoorrtthhccooddeerrss')).toEqual(['n', 'o', 'r', 't', 'h', 'c', 'o', 'd', 'e', 'r', 's']);
   })
 })
--- unique-and-ordered.js
+++ unique-and-ordered.js
@@ -1,6 +1,7 @@
 // Please do not change the name of this function
-const uniqueAndOrdered = () => {
-  // Your code here
+const uniqueAndOrdered = (input) => {
+  if (typeof input === 'string') input = input.split('')
+  return input.filter((element, index) => element !== input[index - 1]);
 }
 
 module.exports = { uniqueAndOrdered }
