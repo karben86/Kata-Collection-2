ac7c32662c8a02444f7e1a6cbc850d5a19049ec4
karben86 <benjaminkarst86@gmail.com>
2020-10-30 09:14:39 +0000

    solved sumConsecutiveDuplicates problem

--- __tests__/sum-consecutive-duplicates.test.js
+++ __tests__/sum-consecutive-duplicates.test.js
@@ -2,6 +2,15 @@ const { sumConsecutiveDuplicates } = require('../sum-consecutive-duplicates');
 
 describe('sumConsecutiveDuplicates()', () => {
   it('', () => {
-    // first test here
+    expect(sumConsecutiveDuplicates([1, 2, 3])).toEqual([1, 2, 3])
+  });
+  it('', () => {
+    expect(sumConsecutiveDuplicates([1, 1, 2, 3])).toEqual([2, 2, 3])
+  });
+  it('', () => {
+    expect(sumConsecutiveDuplicates([1, 1, 2, 2, 3, 3])).toEqual([2, 4, 6])
+  });
+  it('', () => {
+    expect(sumConsecutiveDuplicates([1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1])).toEqual([2, 2, 4, 2, 3])
   });
 });
