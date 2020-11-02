3bcca143d78658e3ed4a235cc971499efb0b0ef5
karben86 <benjaminkarst86@gmail.com>
2020-11-02 15:24:34 +0000

    solved fold-arrays problem

--- __tests__/fold-array.test.js
+++ __tests__/fold-array.test.js
@@ -2,6 +2,27 @@ const { foldArray } = require('../fold-array')
 
 describe('foldArray()', () => {
   it('', () => {
-    // first test here
+    expect(foldArray([1, 2], 1)).toEqual([3]);
+  })
+  it('', () => {
+    expect(foldArray([1, 2, 3], 1)).toEqual([4, 2]);
+  })
+  it('', () => {
+    expect(foldArray([1,2,3,4,5], 1)).toEqual([6,6,3]);
+  })
+  it('', () => {
+    expect(foldArray([1, 2, 3, 10, 34, 100], 1)).toEqual([101, 36, 13]);
+  })
+  it('', () => {
+    expect(foldArray([1, 2, 3, 10, 34, 100], 2)).toEqual([114, 36]);
+  })
+  it('', () => {
+    expect(foldArray([1, 2, 3, 10, 34, 100], 3)).toEqual([150]);
+  })
+  it('', () => {
+    expect(foldArray([1, 2, 3, 10, 34, 100], 4)).toEqual([150]);
+  })
+  it('', () => {
+    expect(foldArray([1, 2, 3, 4, 5, 6, 7, 8, 9], 2)).toEqual([15, 20, 10]);
   })
 })
--- fold-array.js
+++ fold-array.js
@@ -1,6 +1,15 @@
 // Please do not change the name of this function
-const foldArray = (arr) => {
-  // Your code here
+const foldArray = (arr, depth) => {
+  let newArr = []; 
+  
+  for (j = 0; j < depth; j++){
+    for (let i = 0; i < arr.length/2; i++){
+      newArr.push(arr[i] + arr[arr.length - i - 1] * (i + 0.5 !== arr.length/2));
+    }
+    arr = newArr;
+    newArr = [];
+  }
+  return arr;
 }
 
 module.exports = { foldArray }
