ad2e11d94c77aeff05e5f30d085bb92b6e1791a0
karben86 <benjaminkarst86@gmail.com>
2020-11-03 09:34:37 +0000

    solved find closing parenthesis

--- __tests__/find-closing-parenthesis.test.js
+++ __tests__/find-closing-parenthesis.test.js
@@ -2,6 +2,18 @@ const { findClosingParenthesis } = require('../find-closing-parenthesis')
 
 describe('findClosingParenthesis()', () => {
   it('', () => {
-    // first test here
+    expect(findClosingParenthesis('')).toBe(-1);
+  })
+  it('', () => {
+    expect(findClosingParenthesis('hello')).toBe(-1);
+  })
+  it('', () => {
+    expect(findClosingParenthesis('(hello)', 1)).toBe(6);
+  })
+  it('', () => {
+    expect(findClosingParenthesis('Hello, (world). (Something Else)', 2)).toBe(31);
+  })
+  it('', () => {
+    expect(findClosingParenthesis('Hello, (world, (foo) bar (something) else), foo (bar) cat', 3)).toBe(41);
   })
 })
--- find-closing-parenthesis.js
+++ find-closing-parenthesis.js
@@ -1,6 +1,7 @@
 // Please do not change the name of this function
-const findClosingParenthesis = (str) => {
-  // Your code here
+const findClosingParenthesis = (str, n) => {
+  for (let i = 0; i < n - 1; i++) str = str.replace(')', '}')
+  return str.indexOf(')')
 }
 
 module.exports = { findClosingParenthesis }
